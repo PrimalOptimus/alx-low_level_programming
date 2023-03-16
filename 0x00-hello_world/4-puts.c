9fef41209107625e47afab06126a2cd78680cd81
PrimalOptimus <ludgeryp@gmail.com>
2023-03-16 07:17:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /* Main Function */
-int main(void)
-{
-	puts("Programming is like a multilingual language");
+ int main(void)
+ {
+	puts("\"Programming is like a multilingual language");
 		return(0);
-}
+ }
 
