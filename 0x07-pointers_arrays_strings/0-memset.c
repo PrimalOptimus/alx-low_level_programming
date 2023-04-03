c64bc6191d3e9d3dad591511f70f53fc48d09fba
PrimalOptimus <ludgeryp@gmail.com>
2023-04-03 05:56:51 -0700

    Hello Ponter

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/**
+ * _memset -fills a memory with a spesific value
+ * @s:  -starting address of a memory
+ * @b: -value wanted
+ * @n: -intgers to be changed
+ * Return: - return a changed array
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+int i = 0;
+
+for (; n > 0, i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+
+
+}
