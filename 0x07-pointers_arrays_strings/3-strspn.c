9658f2dcc36e32f3ee64ff2d776917374c9bdca0
PrimalOptimus <ludgeryp@gmail.com>
2023-04-03 15:29:47 -0700

    string

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - Function thats an Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int j = 0, i = 0;
+	unsigned int a = 0;
+
+	while (s[i] != ' ' && s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+				a++;
+			j++;
+		}
+		i++;
+		j = 0;
+	}
+	return (a);
+}
