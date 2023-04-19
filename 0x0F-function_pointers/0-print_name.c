8ff54b6d005a1f947062f402c71287db4c984257
PrimalOptimus <ludgeryp@gmail.com>
2023-04-19 11:08:31 -0700

    Hello

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - function that print a name
+ *
+ *@name: first vslur
+ *
+ * @f: function
+ *
+ *
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+
+	f(name);
+}
