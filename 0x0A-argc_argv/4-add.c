d5ba0c5dd6fc3e94aca152ff3ad1cf411235a757
PrimalOptimus <ludgeryp@gmail.com>
2023-04-22 09:47:54 -0700

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - adds two positive number
+ * @argc: number of arguments
+ * @argv: array of arguents
+ *
+ * Return: 0 (Success), or 1 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int x, y;
+	int result;
+
+	result = 0;
+
+	for (x = 1; x < argc; x++)
+	{
+		for (y = 0; argv[x][y] != '\0'; y++)
+		{
+			if (argv[x][y] < '0' || argv[x][y] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+
+		}
+
+		result += _atoi(argv[y]);
+
+	}
+
+	printf("%d\n", result);
+
+	return (0);
+}
