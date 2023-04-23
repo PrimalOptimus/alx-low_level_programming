1708f87a53306b0b2b25ff5dac24680ae34cae51
PrimalOptimus <ludgeryp@gmail.com>
2023-04-23 05:14:44 -0700

    o

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dog - this is a structure of a dog.
+ * @name: the name of the dog
+ * @age: the name of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: this structure defines some characteristics that a dog has.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
