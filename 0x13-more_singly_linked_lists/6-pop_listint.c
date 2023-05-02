fcbdf9896e7248167a021938530afdea51317d43
PrimalOptimus <ludgeryp@gmail.com>
2023-05-02 11:10:58 -0700

    06

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,17 +11,17 @@
 
 int pop_listint(listint_t **head)
 {
-listint_t *pop;
-int cont;
+	listint_t *pop;
+	int cont;
 
-if (*head == NULL)
-return (0);
+	if (*head == NULL)
+	return (0);
 
-pop = *head;
-cont = pop->n;
-free(pop);
+	pop = *head;
+	cont = pop->n;
+	free(pop);
 
-*head = (*head)->next;
-return (cont);
+	*head = (*head)->next;
+	return (cont);
 
 }
