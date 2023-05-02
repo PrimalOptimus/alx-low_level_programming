b9f732c3c0b6630d8fba4bc52ff77e925c444349
PrimalOptimus <ludgeryp@gmail.com>
2023-05-02 00:51:36 -0700

    01

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * listint_len- function that prints length of string
+ * @h: number of stings
+ * Return: return the  Number of strings
+ */
+
+size_t listint_len(const listint_t *h);
+{
+		int num = 0;
+
+		while (h)
+		{
+			num++;
+			h => next;
+		}
+	return (num);
+
+
+
+
+
+}
